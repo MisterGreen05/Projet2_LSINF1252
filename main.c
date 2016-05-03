e50a32e150ed5071fc5b4650a1686f82269f906c
A.M.S <amamanpreet.singh@student.uclouvain.be>
2016-05-03 20:18:07 +0200

    Presque fonctionnel

--- main.c
+++ main.c
@@ -6,77 +6,42 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-static unsigned int maxthreads = 1;
+#include "libfractal/fractal.h"
+
 typedef enum {false, true} bool;
 
 typedef struct{
   struct fractal** buf;
   sem_t full;
   sem_t empty;
+  unsigned int maxthreads;
+  int optionD;
+  int numberfiles;
+  FILE* fileToBeRead;
+  char** inputNames;
+  char* fractalFromCommandLine;
+  int IDconsumer;
+  int ID;
+  char* outputName;
   int nextin;
   int nextout;
   sem_t pmut;
   sem_t cmut;
 } buffer_t;
 
-static buffer_t bstruct;
-
 void error(int err, char*msg){
   fprintf(stderr, "%s a retourné %d, message d'erreur : ", msg, err, strerror(errno));
   exit(EXIT_FAILURE);
 }
 
-void init(unsigned int bsize){
-  int ret;
-
-  ret = sem_init(&bstruct.full, 0, 0);
-    if(ret!=0)
-      error(ret,"sem_init");
-
-  ret = sem_init(&bstruct.empty, 0, maxthreads);
-    if(ret!=0)
-      error(ret,"sem_init");
-
-  ret = sem_init(&bstruct.pmut, 0, 1);
-    if(ret!=0)
-      error(ret,"sem_init");
-
-  ret = sem_init(&bstruct.cmut, 0, 1);
-    if(ret!=0)
-      error(ret,"sem_init");
-
-  bstruct.nextin = bstruct.nextout = 0;
-  bstruct.buf = malloc(bsize*(sizeof(struct fractal*)));
-}
-
-void destroy (void){
-  int err;
-
-  err = sem_destroy(&bstruct.empty);
-    if(err!=0)
-      error(err,"sem_destroy");
-
-  err = sem_destroy(&bstruct.full);
-    if(err!=0)
-      error(err,"sem_destroy");
-
-  err = sem_destroy(&bstruct.cmut);
-    if(err!=0)
-      error(err,"sem_destroy");
-
-  err =sem_destroy(&bstruct.pmut);
-    if(err!=0)
-      error(err,"sem_destroy");
-
-}
-
-void* produce(void* arg){
-  FILE * file = (FILE*)arg;
+void* producer(void* arg){
+  buffer_t *buffer = (buffer_t*)arg;
   char *eof;
   char line[512];
-
-  if(file != NULL){
-    while((eof = fgets(line, 192, file)) != NULL){
+  for(buffer->ID ; buffer->ID < buffer->numberfiles ; buffer->ID ++){
+    buffer->fileToBeRead = fopen(buffer->inputNames[buffer->ID], "r");
+  if(buffer->fileToBeRead != NULL){
+    while((eof = fgets(line, 192, buffer->fileToBeRead)) != NULL){
         if ((strdup(eof))[0] == '#' || (strdup(eof))[0] == '\n') {}
 
         else if (strlen(strdup(eof)) < 1) {}
@@ -99,226 +64,232 @@ void* produce(void* arg){
           double b;
           sscanf(fra[3], "%lf", &a);
           sscanf(fra[4], "%lf", &b);
-          struct fractal* f;
-          f = fractal_new(name, width, height, a, b);
+          struct fractal* f =  fractal_new(name, width, height, a, b);
 
-          sem_wait(&bstruct.empty);
-          sem_wait(&bstruct.pmut);
+          sem_wait(&buffer->empty);
+          sem_wait(&buffer->pmut);
 
-          bstruct.buf[bstruct.nextin] = f;
-          bstruct.nextin++;
-          bstruct.nextin %= maxthreads;
+          buffer->buf[buffer->nextin] = f;
+          //printf("ID : %d a creer : %s\n", buffer->ID, fractal_get_name(buffer->buf[buffer->nextin]) );
+          buffer->nextin++;
+          buffer->nextin %= buffer->maxthreads;
 
-          sem_post(&bstruct.pmut);
-          sem_post(&bstruct.full);
+          sem_post(&buffer->pmut);
+          sem_post(&buffer->full);
         }
-    }
+      }
   }
-  fclose(file);
-  return(NULL);
+  fclose(buffer->fileToBeRead);
+  }
+  pthread_exit(NULL);
 }
 
-void* consume(void* arg){
-  struct fractal *f;
-  sem_wait(&bstruct.full);
-  sem_wait(&bstruct.cmut);
+void* consumer(void* arg){
+  buffer_t *buffer = (buffer_t*)arg;
+  sem_wait(&buffer->full);
+  sem_wait(&buffer->cmut);
 
-  f=bstruct.buf[bstruct.nextout];
-
-  int values [(fractal_get_width(f)+1)*(fractal_get_height(f)+1)];
-  int l = 0;
-  int x = 0;
-  int y = 0;
-  while(l < (fractal_get_width(f)+1)*(fractal_get_height(f)+1) ){
-    if(y <= fractal_get_height(f)){
-      values[l] = fractal_compute_value(f,x,y);
-      l++;
-      y++;
-    }
-    else{
-      x++;
-      y=0;
-    }
+struct fractal *f = buffer->buf[buffer->nextout];
+//printf("Consommateur %d a consomme %s\n", buffer->IDconsumer, fractal_get_name(f));
+int values [(fractal_get_width(f)+1)*(fractal_get_height(f)+1)];
+int l = 0;
+int x = 0;
+int y = 0;
+while(l < (fractal_get_width(f)+1)*(fractal_get_height(f)+1) ){
+  if(y <= fractal_get_height(f)){
+    values[l] = fractal_compute_value(f,x,y);
+    l++;
+    y++;
   }
-  if(write_bitmap_sdl(f, fractal_get_name(f)) == -1)
-    printf("FAIL to generate BMP for the fractal");
-
-  bstruct.nextout++;
-  bstruct.nextout %= maxthreads;
-
-  sem_post(&bstruct.cmut);
-  sem_post(&bstruct.empty);
-  return(NULL);
+  else{
+    x++;
+    y=0;
+  }
 }
+if(write_bitmap_sdl(f, fractal_get_name(f)) == -1)
+  printf("FAIL to generate BMP for the fractal");
 
+  buffer->nextout++;
+  buffer->nextout %= buffer->maxthreads;
+
+  sem_post(&buffer->cmut);
+  sem_post(&buffer->empty);
+
+pthread_exit(NULL);
+}
 
 int main(int argc, char* argv[]){
+
+  buffer_t* bstruct = (buffer_t*)malloc(sizeof(buffer_t));
+
+  bstruct->maxthreads = 1;
+  bstruct->optionD = false;
+  bstruct->numberfiles = 0;
+
   char arg1[] = "--maxthreads";
   char arg2[] = "-d";
   char arg3[] = "-";
+
   int argvIndex = 1;
-  int optionD = false;
-  char** inputNames;
-  int fileToRead = 0;
-  char* fractalFromCommandLine;
-  char* outputName;
 
   if(strcmp(argv[1],arg1) == 0 && strcmp(argv[3],arg2) == 0){
-    maxthreads = (unsigned int)atoi(argv[2]);
-    optionD = true;
-    outputName = malloc(512*sizeof(char));
-    outputName = argv[argc-1];
+    bstruct->maxthreads = (unsigned int)atoi(argv[2]);
+    bstruct->optionD = true;
+    bstruct->outputName = (char*)malloc(512*sizeof(char));
+    bstruct->outputName = argv[argc-1];
 
-    argvIndex = 4;
-    inputNames = malloc((argc-argvIndex)*sizeof(char*));
+    bstruct->inputNames = (char**)malloc((argc-argvIndex)*sizeof(char*));
 
-    for(argvIndex ; argvIndex < argc-1 ; argvIndex++){
+    for(argvIndex = 4 ; argvIndex < argc-1 ; argvIndex++){
       if(strcmp(argv[argvIndex],arg3) != 0){
-        inputNames[fileToRead] = (char *)malloc(512*sizeof(char));
-        inputNames[fileToRead] = argv[argvIndex];
-        fileToRead++;
+        bstruct->inputNames[bstruct->numberfiles] = (char *)malloc(512*sizeof(char));
+        bstruct->inputNames[bstruct->numberfiles] = argv[argvIndex];
+        bstruct->numberfiles++;
       }
       else{
         printf("Vous avez choisi l'option - donc rentrez les données de la fractal : \n");
-        fractalFromCommandLine = malloc(512*sizeof(char));
-        scanf("%s", fractalFromCommandLine);
+        bstruct->fractalFromCommandLine = (char*)malloc(512*sizeof(char));
+        scanf("%s", bstruct->fractalFromCommandLine);
       }
     }
   }
 
   else if(strcmp(argv[1],arg2) == 0 && strcmp(argv[2],arg1) == 0){
-    maxthreads = (unsigned int)atoi(argv[3]);
-    optionD = true;
-    outputName = malloc(512*sizeof(char));
-    outputName = argv[argc-1];
+    bstruct->maxthreads = (unsigned int)atoi(argv[3]);
+    bstruct->optionD = true;
+    bstruct->outputName = (char*)malloc(512*sizeof(char));
+    bstruct->outputName = argv[argc-1];
 
-    argvIndex = 4;
-    inputNames = malloc((argc-argvIndex)*sizeof(char*));
+    bstruct->inputNames = (char**)malloc((argc-argvIndex)*sizeof(char*));
 
-    for(argvIndex ; argvIndex < argc-1 ; argvIndex++){
+    for(argvIndex = 4; argvIndex < argc-1 ; argvIndex++){
       if(strcmp(argv[argvIndex],arg3) != 0){
-        inputNames[fileToRead] = (char *)malloc(512*sizeof(char));
-        inputNames[fileToRead] = argv[argvIndex];
-        fileToRead++;
+        bstruct->inputNames[bstruct->numberfiles] = (char *)malloc(512*sizeof(char));
+        bstruct->inputNames[bstruct->numberfiles] = argv[argvIndex];
+        bstruct->numberfiles++;
       }
       else{
         printf("Vous avez choisi l'option - donc rentrez les données de la fractal : \n");
-        fractalFromCommandLine = malloc(512*sizeof(char));
-        scanf("%s", fractalFromCommandLine);
+        bstruct->fractalFromCommandLine = (char*)malloc(512*sizeof(char));
+        scanf("%s", bstruct->fractalFromCommandLine);
       }
     }
   }
 
   else if(strcmp(argv[1],arg1) == 0 && strcmp(argv[3],arg2) !=0){
-    maxthreads = (unsigned int)atoi(argv[2]);
-    optionD = false;
-    outputName = malloc(512*sizeof(char));
-    outputName = argv[argc-1];
+    bstruct->maxthreads = (unsigned int)atoi(argv[2]);
+    bstruct->optionD = false;
+    bstruct->outputName = (char*)malloc(512*sizeof(char));
+    bstruct->outputName = argv[argc-1];
 
-    argvIndex = 3;
-    inputNames = malloc((argc-argvIndex)*sizeof(char*));
+    bstruct->inputNames = (char**)malloc((argc-argvIndex)*sizeof(char*));
 
-    for(argvIndex ; argvIndex < argc-1 ; argvIndex++){
+    for(argvIndex = 3 ; argvIndex < argc-1 ; argvIndex++){
       if(strcmp(argv[argvIndex],arg3) != 0){
-        inputNames[fileToRead] = (char *)malloc(512*sizeof(char));
-        inputNames[fileToRead] = argv[argvIndex];
-        fileToRead++;
+        bstruct->inputNames[bstruct->numberfiles] = (char *)malloc(512*sizeof(char));
+        bstruct->inputNames[bstruct->numberfiles] = argv[argvIndex];
+        bstruct->numberfiles++;
       }
       else{
         printf("Vous avez choisi l'option - donc rentrez les données de la fractal : \n");
-        fractalFromCommandLine = malloc(512*sizeof(char));
-        scanf("%s", fractalFromCommandLine);
+        bstruct->fractalFromCommandLine = (char*)malloc(512*sizeof(char));
+        scanf("%s", bstruct->fractalFromCommandLine);
       }
     }
   }
 
   else if(strcmp(argv[1],arg2) == 0 && strcmp(argv[2],arg1) !=0){
-    optionD = true;
-    outputName = malloc(512*sizeof(char));
-    outputName = argv[argc-1];
+    bstruct->optionD = true;
+    bstruct->outputName = (char*)malloc(512*sizeof(char));
+    bstruct->outputName = argv[argc-1];
 
-    argvIndex = 2;
-    inputNames = malloc((argc-argvIndex)*sizeof(char*));
+    bstruct->inputNames = (char**)malloc((argc-argvIndex)*sizeof(char*));
 
-    for(argvIndex ; argvIndex < argc-1 ; argvIndex++){
+    for(argvIndex = 2 ; argvIndex < argc-1 ; argvIndex++){
       if(strcmp(argv[argvIndex],arg3) != 0){
-        inputNames[fileToRead] = (char *)malloc(512*sizeof(char));
-        inputNames[fileToRead] = argv[argvIndex];
-        fileToRead++;
+        bstruct->inputNames[bstruct->numberfiles] = (char *)malloc(512*sizeof(char));
+        bstruct->inputNames[bstruct->numberfiles] = argv[argvIndex];
+        bstruct->numberfiles++;
       }
       else{
         printf("Vous avez choisi l'option - donc rentrez les données de la fractal : \n");
-        fractalFromCommandLine = malloc(512*sizeof(char));
-        scanf("%s", fractalFromCommandLine);
+        bstruct->fractalFromCommandLine = (char*)malloc(512*sizeof(char));
+        scanf("%s", bstruct->fractalFromCommandLine);
       }
     }
   }
 
   else if(argvIndex == 1){
-    optionD = false;
-    outputName = malloc(512*sizeof(char));
-    outputName = argv[argc-1];
+    bstruct->optionD = false;
+    bstruct->outputName = (char*)malloc(512*sizeof(char));
+    bstruct->outputName = argv[argc-1];
 
-    inputNames = malloc((argc-argvIndex)*sizeof(char*));
+    bstruct->inputNames = (char**)malloc((argc-argvIndex)*sizeof(char*));
 
-    for(argvIndex ; argvIndex < argc-1 ; argvIndex++){
+    for(argvIndex = 1 ; argvIndex < argc-1 ; argvIndex++){
       if(strcmp(argv[argvIndex],arg3) != 0){
-        inputNames[fileToRead] = (char *)malloc(512*sizeof(char));
-        inputNames[fileToRead] = argv[argvIndex];
-        fileToRead++;
+        bstruct->inputNames[bstruct->numberfiles] = (char *)malloc(512*sizeof(char));
+        bstruct->inputNames[bstruct->numberfiles] = argv[argvIndex];
+        bstruct->numberfiles++;
       }
       else{
         printf("Vous avez choisi l'option - donc rentrez les données de la fractal : \n");
-        fractalFromCommandLine = malloc(512*sizeof(char));
-        scanf("%s", fractalFromCommandLine);
+        bstruct->fractalFromCommandLine = (char*)malloc(512*sizeof(char));
+        scanf("%s", bstruct->fractalFromCommandLine);
       }
     }
   }
 
   else{
-    int err;
-    error(err,"L'ensemble d'instructions pour vérifier le format d'entrée ");
-    error(err,"sem_destroy");
+    int err = -1;
+    printf("Erreur formate d'entrée : %d\n", err);
   }
 
-  /*Partie thread*/
+  /* THREAD PART */
 
-  pthread_t threadLecture[fileToRead];
-  pthread_t threadCalcul[maxthreads];
+  pthread_t threadLecture;
+  pthread_t threadCalcul[bstruct->maxthreads];
 
-  init(maxthreads);
+  int ret;
+
+  ret = sem_init(&bstruct->full, 0, 0);
+    if(ret!=0)
+      error(ret,"sem_init");
+
+  ret = sem_init(&bstruct->empty, 0, bstruct->maxthreads);
+    if(ret!=0)
+      error(ret,"sem_init");
+
+  ret = sem_init(&bstruct->pmut, 0, 1);
+    if(ret!=0)
+      error(ret,"sem_init");
+
+  ret = sem_init(&bstruct->cmut, 0, 1);
+    if(ret!=0)
+      error(ret,"sem_init");
+
+  bstruct->buf = (struct fractal**)malloc(bstruct->maxthreads * sizeof(struct fractal*));
+
+  bstruct->nextin = 0;
+  bstruct->nextout = 0;
 
   int err;
 
-  for(int i = 0 ; i < fileToRead ; i++){
-    err = pthread_create(&(threadLecture[i]), NULL, produce,(void*)fopen(inputNames[i],"r"));
+  err = pthread_create(&threadLecture, NULL, producer, (void*)bstruct);
     if(err!=0)
       error(err,"pthread_create");
+
+  for(bstruct->IDconsumer = 0 ; bstruct->IDconsumer < bstruct->maxthreads ; bstruct->IDconsumer++){
+    err = pthread_create(&threadCalcul[bstruct->IDconsumer], NULL, consumer, (void*)bstruct);
+      if(err!=0)
+        error(err,"pthread_create");
   }
 
-  for(int i = 0 ; i < maxthreads ; i++){
-  err = pthread_create(&(threadCalcul[i]), NULL, consume, NULL);
-  if(err!=0)
-    error(err,"pthread_create");
+  ret = pthread_join(threadLecture, NULL);
+
+  for(int i = 0 ; i < bstruct->maxthreads ; i++){
+    ret = pthread_join(threadCalcul[i], NULL);
   }
 
-  for(int i=0 ; i < fileToRead ; i++) {
-    void *r;
-    err=pthread_join(threadLecture[i],(void **)&r);
-    free(r);
-    if(err!=0)
-      error(err,"pthread_join");
-  }
-
-  for(int i=0 ; i < maxthreads ; i++) {
-    void *r;
-    err=pthread_join(threadCalcul[i],(void **)&r);
-    free(r);
-    if(err!=0)
-      error(err,"pthread_join");
-  }
-
-  destroy();
-
-  return(EXIT_SUCCESS);
+  return 0;
 }
